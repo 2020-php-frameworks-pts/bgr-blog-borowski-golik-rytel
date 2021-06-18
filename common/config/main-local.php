2e43627ed3efaf6e3beb64787ff6e39550546867
Wojciech Borowski <61619367+WBorowski91@users.noreply.github.com>
2021-06-18 22:04:26 +0200

    konfiguracja konta pocztowego na potrzeby wysyłania maili aktywacyjnych, drobne poprawki w ścieżkach

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -10,11 +10,15 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'viewPath' => '@app/mailer',
+            'useFileTransport' => false,
+            'transport' => [
+            'class' => 'Swift_SmtpTransport',
+            'host' => 'smtp.gmail.com',
+            'username' => 'jk6629724@gmail.com',
+            'password' => '-zyrafa142',
+            'port' => '587',
+            'encryption' => 'tls',
         ],
     ],
 ];
